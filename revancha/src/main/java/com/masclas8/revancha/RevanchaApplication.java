65461d9cec5e600128c742d26e82ecfc67176499
lartjoni <lartoniron@gmail.com>
2023-05-02 21:40:20 -0300

    creado repositrio git y enviando a hub

---
+++ revancha/src/main/java/com/masclas8/revancha/RevanchaApplication.java
@@ -0,0 +1,13 @@
+package com.masclas8.revancha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RevanchaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RevanchaApplication.class, args);
+	}
+
+}
---
+++ revancha/src/test/java/com/masclas8/revancha/RevanchaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.masclas8.revancha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RevanchaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
